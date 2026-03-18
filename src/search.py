28e43240a579357e2515a81747b516c9c4ba9aaf
jpbusti <jpbustillo@uninorte.edu.co>
2026-03-18 12:49:22 -0500

    Update search.py

---
+++ src/search.py
@@ -0,0 +1,13 @@
+from node import Node
+
+def get_all_nodes(node, result=None):
+    if result is None:
+        result = []
+    if node is None:
+        return result
+    
+    get_all_nodes(node.left, result)   
+    result.append(node)                
+    get_all_nodes(node.right, result)  
+    
+    return result
